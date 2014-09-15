3eaf20b0f23def671cc50914cdebc2dde6ee1317
tengkaixuan <tengkaixuan@126.com>
2014-09-15 10:55:29 +0800

    #3 Add missed file

---
+++ Core/Model/Part.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Model
+{
+  public class Part
+  {
+    public string Operator { get; set; }
+    public string Supplier { get; set; }
+    public string PartName { get; set; }
+    public string PartCadNumber { get; set; }
+    public int TotalCount { get; set; }
+    public int SampleCount { get; set; }
+    public List<DimensionEntity> DimensionEntitys { get; set; }
+  }
+}
