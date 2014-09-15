1cb3d7036b8945be6e537be6df5b50ee6e48a01d
tengkaixuan <tengkaixuan@126.com>
2014-09-15 19:45:59 +0800

    #3 Set edit to Unicode made by VS

--- TX_PMS/Properties/AssemblyInfo.cs
+++ TX_PMS/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-// Copyright © 2009-2010, Open Design Alliance (the "Alliance") 
+// Copyright ?2009-2010, Open Design Alliance (the "Alliance") 
 // 
 // This software is owned by the Alliance, and may only be incorporated into 
 // application programs owned by members of the Alliance subject to a signed 
@@ -10,12 +10,12 @@
 // programs incorporating this software must include the following statement 
 // with their copyright notices:
 //
-// Teigha™.NET for .dwg files 2009-2010 by Open Design Alliance. All rights reserved.
+// Teigha?NET for .dwg files 2009-2010 by Open Design Alliance. All rights reserved.
 //
 // By use of this software, you acknowledge and accept these terms.
 //
 //
-// *DWG is the native and proprietary file format for AutoCAD® and a trademark 
+// *DWG is the native and proprietary file format for AutoCAD?and a trademark 
 // of Autodesk, Inc. The Open Design Alliance is not associated with Autodesk.
 ///////////////////////////////////////////////////////////////////////////////
 using System.Reflection;
@@ -26,11 +26,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Teigha™.NET for .dwg files: OdViewExMgd sample")] // FILE_DESCRIPTION
-[assembly: AssemblyDescription("Teigha™.NET for .dwg files 3.3.0.0")]
+[assembly: AssemblyTitle("Teigha?NET for .dwg files: OdViewExMgd sample")] // FILE_DESCRIPTION
+[assembly: AssemblyDescription("Teigha?NET for .dwg files 3.3.0.0")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Open Design Alliance Inc. ('Open Design')")]
-[assembly: AssemblyProduct("Teigha™.NET for .dwg files")]
+[assembly: AssemblyProduct("Teigha?NET for .dwg files")]
 [assembly: AssemblyCopyright("Copyright 2009-2010, Open Design Alliance Inc. ('Open Design')")] 
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
